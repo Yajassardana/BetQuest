2dec1e5455e6ee73d5c066c92e8276df058ac4bf
Yajassardana <yajassardana@gmail.com>
2024-12-07 14:33:32 +0530

    Fix dependencies + add telegram window variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+'use client';
 import Image from "next/image";
+import { useEffect } from 'react';
+declare global {
+  interface Window {
+    Telegram: {
+      WebApp: {
+        ready: () => void;
+        initData?: string;
+        [key: string]: any;
+      };
+    };
+  }
+}
 
 export default function Home() {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.Telegram) {
+      window.Telegram.WebApp.ready();
+      console.log("Telegram app ready", window.Telegram);
+    }
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
